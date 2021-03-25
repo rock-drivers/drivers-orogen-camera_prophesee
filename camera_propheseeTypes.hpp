513dd8309115f69a011895f4a0653fd8c94e7e5d
Javier Hidalgo-Carrió <havyhidalgo@gmail.com>
2021-03-25 19:49:43 +0100

    types: prophesee types

--- camera_propheseeTypes.hpp
+++ camera_propheseeTypes.hpp
@@ -1,6 +1,8 @@
 #ifndef camera_prophesee_TYPES_HPP
 #define camera_prophesee_TYPES_HPP
 
+#include <base/Time.hpp>
+
 /* If you need to define types specific to your oroGen components, define them
  * here. Required headers must be included explicitly
  *
@@ -8,7 +10,26 @@
  * which case you do not need this file
  */
 
-namespace camera_prophesee {
+namespace camera_prophesee
+{
+    struct Event
+    {
+        unsigned int x;
+        unsigned int y;
+        ::base::Time ts;
+        bool polarity;
+    };
+
+    struct EventArray
+    {
+        ::base::Time time;
+        unsigned int height;
+        unsigned int width;
+
+        std::vector<Event> events;
+    };
+
+
 }
 
 #endif
