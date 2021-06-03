9fb6971dbddccd3b22fc9cd17d51c20e3c13024d
jhidalgocarrio <havyhidago@gmail.com>
2021-06-03 14:52:28 +0200

    script: driver run ruby file

--- scripts/run.rb
+++ scripts/run.rb
@@ -4,19 +4,36 @@ include Orocos
 
 ENV['PKG_CONFIG_PATH'] = "#{File.expand_path("..", File.dirname(__FILE__))}/build:#{ENV['PKG_CONFIG_PATH']}"
 
+Orocos::CORBA::max_message_size = 100000000000
 Orocos.initialize
 
-Orocos::Process.run 'camera_prophesee::Task' => 'camera_prophesee' do
-    # log all the output ports
-    #Orocos.log_all_ports 
+Orocos::Process.run 'camera_prophesee::Task' => 'camera_prophesee',
+                    'camera_prophesee::FrameGenerator' => 'frame_generator' do
+
     Orocos.conf.load_dir('../config')
 
     # Get the task
     driver = Orocos.name_service.get 'camera_prophesee'
     Orocos.conf.apply(driver, ['default','prophesee_yellow_dot'], :override => true)
 
+    # Get the frame generator
+    generator = Orocos.name_service.get 'frame_generator'
+    Orocos.conf.apply(generator, ['default'], :override => true)
+
+    # Configure
     driver.configure
+    generator.configure
+
+    # Connect ports
+    driver.events.connect_to generator.events, :type => :buffer, :size => 2
+
+    # log all the output ports
+    Orocos.log_all_ports 
+ 
+    # Start
     driver.start
+    generator.start
+
 
     Vizkit.exec
 end
